360f4015e9fd0a86c942e7c014c2205d5b5cda9e
higgz <hox@dildomail.com>
2012-10-31 16:47:04 +0000

    Create Repository Init an Ant Environment You need to run android update from the sdk tools to recreate local.properties with your shit Add an activity with two butans and a surfaceview Add handing for the camera preview

---
+++ bin/classes.dex.d
@@ -0,0 +1,11 @@
+/home/hex/Shitpreview/bin/classes.dex : \
+/home/hex/Shitpreview/bin/classes/org/buttes/shitpreview/preview.class \
+/home/hex/Shitpreview/bin/classes/org/buttes/shitpreview/R$id.class \
+/home/hex/Shitpreview/bin/classes/org/buttes/shitpreview/R$string.class \
+/home/hex/Shitpreview/bin/classes/org/buttes/shitpreview/preview$2.class \
+/home/hex/Shitpreview/bin/classes/org/buttes/shitpreview/R.class \
+/home/hex/Shitpreview/bin/classes/org/buttes/shitpreview/BuildConfig.class \
+/home/hex/Shitpreview/bin/classes/org/buttes/shitpreview/R$attr.class \
+/home/hex/Shitpreview/bin/classes/org/buttes/shitpreview/R$layout.class \
+/home/hex/Shitpreview/bin/classes/org/buttes/shitpreview/preview$1.class \
+/home/hex/android-sdks/tools/support/annotations.jar \
---
+++ src/org/buttes/shitpreview/preview.java
@@ -0,0 +1,88 @@
+package org.buttes.shitpreview;
+import java.io.IOException;
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.graphics.PixelFormat;
+import android.hardware.Camera;
+import android.os.Bundle;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.Button;
+
+public class preview extends Activity implements SurfaceHolder.Callback
+{
+	Camera camera;
+	SurfaceView surfaceView;
+	SurfaceHolder surfaceHolder;
+	boolean previewing = false;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+	Button buttonStartCameraPreview = (Button)findViewById(R.id.startcamerapreview);
+	Button buttonStopCameraPreview = (Button)findViewById(R.id.stopcamerapreview);
+
+	getWindow().setFormat(PixelFormat.UNKNOWN);
+	surfaceView = (SurfaceView)findViewById(R.id.surfaceview);
+	surfaceHolder = surfaceView.getHolder();
+	surfaceHolder.addCallback(this);
+	surfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+
+	buttonStartCameraPreview.setOnClickListener(new Button.OnClickListener() {
+		@Override
+		public void onClick(View v) {
+
+			if(!previewing) {
+				camera = Camera.open();
+				if (camera != null) {
+					try {
+						camera.setPreviewDisplay(surfaceHolder);
+						camera.startPreview();
+						previewing = true;
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		}});
+
+	buttonStopCameraPreview.setOnClickListener(new Button.OnClickListener() {
+
+		@Override
+		public void onClick(View v) {
+
+			if(camera !=null && previewing) {
+				camera.stopPreview();
+				camera.release();
+				camera = null;
+
+				previewing = false;
+			}
+		}});
+
+    }
+
+ @Override
+ public void surfaceChanged(SurfaceHolder holder, int format, int width,
+   int height) {
+  // TODO Auto-generated method stub
+  
+ }
+
+ @Override
+ public void surfaceCreated(SurfaceHolder holder) {
+  // TODO Auto-generated method stub
+  
+ }
+
+ @Override
+ public void surfaceDestroyed(SurfaceHolder holder) {
+  // TODO Auto-generated method stub
+  
+ }
+}
