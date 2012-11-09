46ec0c9b49f9b714ec12810b551846e7cd3d8d62
higgz <hox@dildomail.com>
2012-11-09 01:03:58 +0000

    more clean up

--- gen/R.java.d
+++
@@ -1,4 +0,0 @@
-/home/hex/Shitpreview/gen/org/buttes/papertracker/R.java \
- : /home/hex/Shitpreview/res/layout/main.xml \
-/home/hex/Shitpreview/res/values/strings.xml \
-/home/hex/Shitpreview/bin/AndroidManifest.xml \
--- gen/org/buttes/papertracker/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package org.buttes.papertracker;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
